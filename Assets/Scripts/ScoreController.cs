d7a84109e0e09049eb3bf845b6de81ba2d11f75d
João Pedro <joaopedrorib@hotmail.com>
2022-09-14 14:30:04 -0300

    feat(score): auto increase points

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    int score = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    int updateCalls = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        updateCalls++;
+
+        if (updateCalls % 2 == 0) 
+        { 
+            score++;
+            scoreText.text = score.ToString() + " pts";
+        }
+    }
+}
